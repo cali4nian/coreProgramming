82c5a8212f7f22d12f200dd244b5a7f6f64faa32
Daniel Soriano <danielsoriano559@gmail.com>
2025-03-12 19:59:18 -0700

    ...

--- sportsFunnelWebsite/js/index/index.js
+++ sportsFunnelWebsite/js/index/index.js
@@ -1,9 +1,7 @@
 // Imports
 import EmailSubscriber from "../EmailSubscriber.js"
-import BackToTopButton from "../BackToTopButton.js"
 
 // Classes
 document.addEventListener("DOMContentLoaded", () => {
   new EmailSubscriber("subscriberForm", "endpointHere")
-  new BackToTopButton("backToTop")
 })
--- sportsFunnelWebsite/js/terms-of-use/terms-of-use.js
+++
@@ -1,7 +0,0 @@
-// Imports
-import BackToTopButton from "../BackToTopButton.js"
-
-// Classes
-document.addEventListener("DOMContentLoaded", () => {
-  new BackToTopButton("backToTop")
-})

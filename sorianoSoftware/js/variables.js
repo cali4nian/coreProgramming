8b5bec6936414495962246a4ea00e4af36875272
Daniel Soriano <danielsoriano559@gmail.com>
2025-06-19 16:36:11 -0700

    soriano software update site

--- sorianoSoftware/js/variables.js
+++ sorianoSoftware/js/variables.js
@@ -1,8 +1,3 @@
-export const stylesheet = document.getElementById("stylesheet")
-export const homePageStyles = "css/home.css"
-export const servicesPageStyles = "css/services.css"
-export const downloadsPageStyles = "css/downloads.css"
-export const mainNavigation = document.querySelector("body > header > nav > ul")
 export const mainContentContainer = document.getElementById("root")
 export const currentYearSpan = document.getElementById("currentYear")
 export const currentYear = new Date().getFullYear()

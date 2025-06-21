aa208f7c601f7394cbf184e0a2b10d821ca0f2a4
Daniel Soriano <danielsoriano559@gmail.com>
2025-06-21 15:49:09 -0700

    attempt 1 fix autoloader

--- basketBallCoachingAgency_FullStack/app/autoload.php
+++ basketBallCoachingAgency_FullStack/app/autoload.php
@@ -2,19 +2,16 @@
 spl_autoload_register(function ($class) {
     // Remove 'App\' namespace prefix if present
     $class = str_replace('App\\', '', $class);
-
     // Convert namespace to path
     $class = str_replace('\\', '/', $class);
-
-    // Lowercase only 'Controllers' part of the path
-    // $class = preg_replace_callback('/(^|\/)(Controllers|Models|Config|Database)(\/|$)/', function ($matches) {
-    //     return $matches[1] . strtolower($matches[2]) . $matches[3];
-    // }, $class);
-
     // Full file path
     $file = __DIR__ . '/' . $class . '.php';
-
     if (file_exists($file)) {
+        // Enforce case-sensitive match
+        $realFile = realpath($file);
+        if (basename($realFile) !== basename($file)) {
+            die("Autoload error: Case mismatch for class '$class'. Expected file: '$file', found: '$realFile'");
+        }
         require_once $file;
     } else {
         die("Autoload error: Class '$class' not found at '$file'");
